0710cf7ab9faefb88d1252dedde1921c4a2f0b61
Brandon Blair <brandon.d.blair@me.com>
2017-04-01 00:04:47 -0600

    rampNumbers.js completed

---
+++ 02_rampNumbers/solutions/rampNumbers.js
@@ -0,0 +1,17 @@
+let trueInput = 1234;
+let falseInput = 1032;
+
+function rampNumbers(int) {
+  // Create array with each number from integer
+  let numArr = int.toString().split('');
+
+  // Loop through length of array of numbers
+  for (let i = 0; i < numArr.length; i++) {
+    // If current number is in array is larger than the next then return false.
+    if (numArr[i] > numArr[i + 1]) return false;
+  }
+  return true;
+}
+
+console.log(rampNumbers(trueInput));
+console.log(rampNumbers(falseInput));
