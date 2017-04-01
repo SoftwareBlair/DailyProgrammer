3bc185e04f2a337198659119a443115fec808e4a
Brandon Blair <brandon.d.blair@me.com>
2017-04-01 11:59:10 -0600

    palindrom.js challenge completed

---
+++ 03_palindrome/solutions/palindrome.js
@@ -0,0 +1,30 @@
+let string1 = 'A man, a plan, a canal, Panama!'; // True
+let string2 = 'race car'; // True
+let string3 = 'Am I a palindrome?'; // False
+
+function palindrome(str) {
+  let newString = flattenString(str);
+  let stringRev = reverseString(newString);
+
+  // If strings are equal, then it's a Palindrom
+  if (newString === stringRev) return true;
+
+  return false;
+
+}
+
+function flattenString(str) {
+  return str.replace(/[^\w\s]|_/g, '') // Removes Punctuation
+            .replace(/\s/g, '') // Removes Whitespace
+            .toLowerCase();  // Converts all letters to lowercase
+}
+
+function reverseString(str) {
+  return str.split('') // Split string into new array
+            .reverse() // Reverse Array
+            .join(''); // Join reversed array into a string
+}
+
+console.log(palindrome(string1));
+console.log(palindrome(string2));
+console.log(palindrome(string3));
