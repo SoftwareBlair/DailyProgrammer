9a5e0fd0458debd72c36d5dbecf177bbfd0c9ccf
Brandon Blair <brandon.d.blair@me.com>
2017-04-02 17:33:54 -0600

    findGreater.js challenge completed

---
+++ 07_findGreater/solutions/findGreater.js
@@ -0,0 +1,18 @@
+let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function findGreater(arg1, arg2) {
+  let result = [];
+
+  arg1.forEach(num => {
+    if (num > arg2) result.push(num);
+  });
+
+  if (!result.length) result = 'No numbers are greater';
+
+  return result;
+
+}
+
+console.log(findGreater(numArray, 3));
+console.log(findGreater(numArray, 7));
+console.log(findGreater(numArray, 12));
