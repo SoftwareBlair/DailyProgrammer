d330c5c2164c6c3c176699f9b8e914fc9e33b074
Brandon Blair <brandon.d.blair@me.com>
2017-04-03 09:21:23 -0600

    combineAndSort.js challenge completed

---
+++ 08_combineAndSort/solutions/combineAndSort.js
@@ -0,0 +1,17 @@
+let array1 = ['cat', 'dog', 'fish', 'zebra'];
+let array2 = ['lion', 'aardvark', 'gorilla'];
+
+function combineAndSort(arr1, arr2) {
+  let newArr = [];
+
+  // Loop through each array and push into new array
+  arr1.forEach(word => newArr.push(word));
+  arr2.forEach(word => newArr.push(word));
+
+  // Sort array in alphabetical oder
+  let result = newArr.sort();
+
+  return result;
+}
+
+console.log(combineAndSort(array1, array2));
