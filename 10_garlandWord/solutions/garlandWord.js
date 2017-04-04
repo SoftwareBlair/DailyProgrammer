9525a62a2875684fb233f1cd12281ea43b27eb2c
Brandon Blair <brandon.d.blair@me.com>
2017-04-04 17:06:18 -0600

    garlandWord.js challenge completed

--- 10_garlandWord/solutions/garlandWord.js
+++ 10_garlandWord/solutions/garlandWord.js
@@ -4,7 +4,28 @@ const garland3 = 'onion'; // Returns 2
 const garland4 = 'alfalfa'; // Returns 4
 
 function garlandWord(str) {
+  // Set two variables from input to modify and compare
+  let first = str.split('');
+  let last = str.split('');
+  let result = 0;
 
+  // Helper function to modify local strings
+  let shiftPop = () => {
+    first.pop(); // Remove last element of array
+    last.shift(); // Remove first element of array
+    return first.length;
+  }
+
+  // While shiftPop function returns greater than zero
+  while (shiftPop()) {
+
+    // Compare each string, if they match then return the length
+    if (first.join('') === last.join('')) return first.length;
+
+  }
+
+  // Returns 0 if a match is not found in while loop
+  return result;
 }
 
 console.log(garlandWord(garland1));
