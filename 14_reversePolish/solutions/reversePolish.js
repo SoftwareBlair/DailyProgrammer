7da75b3cfd3158c79c5d3c7eef5bd6614cd496ef
Brandon Blair <brandon.d.blair@me.com>
2017-04-05 14:44:34 -0600

    Started reversePolish.js

---
+++ 14_reversePolish/solutions/reversePolish.js
@@ -0,0 +1,12 @@
+const revPolOne = '5 1 2 + 4 x + 3 -'; // Returns 14
+const revPolTwo = '1 2 + 4 5 6 8 * - + +'; // Returns -36
+
+const reversePolish = (str) => {
+  let newArr = str.split(' ');
+  let result;
+
+  return result;
+};
+
+console.log(reversePolish(revPolOne));
+// console.log(reversePolish(revPolTwo));
