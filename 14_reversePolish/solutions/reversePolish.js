e7573bf8db531430ffb6b78f30212ba2bd218421
Brandon Blair <brandon.d.blair@me.com>
2017-04-06 09:48:47 -0600

    reversePolish.js challenge completed

--- 14_reversePolish/solutions/reversePolish.js
+++ 14_reversePolish/solutions/reversePolish.js
@@ -3,10 +3,32 @@ const revPolTwo = '1 2 + 4 5 6 8 * - + +'; // Returns -36
 
 const reversePolish = (str) => {
   let newArr = str.split(' ');
-  let result;
+  let result = [];
+
+  newArr.forEach(elm => {
+
+    if (!isNaN(elm)) result.push(parseInt(elm));
+    else {
+
+      let num2 = result.pop();
+      let num1 = result.pop();
+
+      if (elm === '*' || elm === 'x') result.push(num1 * num2);
+      else
+      if (elm === '/') result.push(num1 / num2);
+      else
+      if (elm === '+') result.push(num1 + num2);
+      else
+      if (elm === '-') result.push(num1 - num2);
+
+    };
+
+  });
+
+  result = result.pop();
 
   return result;
 };
 
 console.log(reversePolish(revPolOne));
-// console.log(reversePolish(revPolTwo));
+console.log(reversePolish(revPolTwo));
