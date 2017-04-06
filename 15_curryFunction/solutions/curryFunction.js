0bfc397d59c27f1e8e750da5c937e9abf6ece63b
Brandon Blair <brandon.d.blair@me.com>
2017-04-06 10:34:42 -0600

    curryFunction.js challenge completed

---
+++ 15_curryFunction/solutions/curryFunction.js
@@ -0,0 +1,6 @@
+const curryFunction = (x) => (y) => x + y;
+
+console.log(curryFunction(1)(1)); // Returns 2
+console.log(curryFunction(20)(20)); // Returns 40
+console.log(curryFunction(100)(5)); // Returns 105
+console.log(curryFunction(40)(2)); // Returns 42
