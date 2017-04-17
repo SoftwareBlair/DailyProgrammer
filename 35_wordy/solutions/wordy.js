079156b01c8dc5c99ceb6832aab68e99a95c66e3
Brandon Blair <brandon.d.blair@me.com>
2017-04-17 16:14:01 -0600

    All tests are passing but last one... and it is annoying the hell out of me

--- 35_wordy/solutions/wordy.js
+++ 35_wordy/solutions/wordy.js
@@ -1,4 +1,24 @@
+const operations = {
+  'plus': (a, b) => a + b,
+  'minus': (a, b) => a - b,
+  'divided': (a, b) => a / b,
+  'multiplied': (a, b) => a * b,
+  'times': (a, b) => a * b
+};
+
 const wordy = (str) => {
+  let operator = str.match(/-?\d+|(plus|minus|multiplied|times|divided)/g);
+  let result = parseInt(operator[0]);
+
+  if (!operator) return 42;
+
+  for (let i = 0; i < operator.length; i++) {
+
+    if (operator[i] in operations) result = operations[operator[i]](result, parseInt(operator[i + 1]));
+
+  };
+
+  return result;
 
 };
 
