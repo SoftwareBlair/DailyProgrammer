ea68bc8ffa866b8c7b810e852b0f5742445f55c4
Brandon Blair <brandon.d.blair@me.com>
2017-04-30 14:57:35 -0600

    Working through jsonFormatting.js

--- 22_jsonFormatting/solutions/jsonFormatting.js
+++ 22_jsonFormatting/solutions/jsonFormatting.js
@@ -1,62 +1,21 @@
-const booksAuthors = {
-  "books": [
-    {
-      "id": 1,
-      "name": "You don't know JS",
-      "author_id": 1,
-      "author_name": "Bonnie"
-    },
-    {
-      "id": 1,
-      "name": "You don't know JS",
-      "author_id": 2,
-      "author_name": "Eiseman"
-    },
-    {
-      "id": 2,
-      "name": "You know some JS",
-      "author_id": 1,
-      "author_name": "Bonnie"
-    }
-  ]
-};
+const booksAuthors = require('../books.json');
 
 const jsonFormatting = (obj) => {
+  const booksArr = obj.data;
+  let book = {};
+      author = {};
+      books = [];
+
+  while (booksArr) {
+
+    
+
+    booksArr.shift();
+
+  }
+
+  return { books }
 
 };
 
 console.log(jsonFormatting(booksAuthors));
-
-/*
-  Should Return
-
-  {
-      "books": [
-        {
-          "id": 1,
-          "name": "You don't know JS",
-          "author": [
-              {
-                  "id": 1,
-                  "name": "Bonnie"
-              },
-              {
-                  "id": 2,
-                  "name": "Eiseman"
-              }
-          ]
-        },
-        {
-          "id": 2,
-          "name": "You know some JS",
-          "author": [
-              {
-                  "id": 1,
-                  "name": "Bonnie"
-              },
-          ]
-        },
-      ]
-  }
-
- */
