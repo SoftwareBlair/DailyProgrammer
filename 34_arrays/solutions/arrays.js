a4eb04f9c8b3fe3ed8c2c96a502fa39623a196d4
Brandon Blair <brandon.d.blair@me.com>
2017-05-08 21:40:52 -0600

    Working through arrays.js challenge

--- 34_arrays/solutions/arrays.js
+++ 34_arrays/solutions/arrays.js
@@ -1,11 +1,29 @@
-function array() {
-  var newArray = {}
-  newArray.length = 0
-  newArray.push = function (value) {
-    //TODO
-  }
-  newArray.pop = function () {
-    //TODO
-  }
-  return newArray
+function Array() {
+    this.length = 0;
 }
+
+Array.prototype.push = function(val) {
+    this[this.length] = val;
+    this.length++;
+}
+
+Array.prototype.pop = function() {
+    if (this.length > 0) {
+        this.length--;
+        var value = this[this.length];
+        delete this.length;
+    }
+    return value;
+}
+
+var arr = new Array();
+arr.push(1);
+console.log(arr.length); // -> 1
+arr.push(10);
+console.log(arr.length); // -> 2
+console.log(arr[0]); // -> 1
+console.log(arr[1]); // -> 10
+console.log(arr.pop()); // -> 10
+console.log(arr.length); // -> 1
+console.log(arr.pop()); // -> 1
+console.log(arr.length); // -> 0
