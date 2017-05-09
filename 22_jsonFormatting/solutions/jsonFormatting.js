227282fbfb8738715d69e5bb54d64256a58e762e
Brandon Blair <brandon.d.blair@me.com>
2017-05-09 15:19:46 -0600

    jsonFormatting.js completed

--- 22_jsonFormatting/solutions/jsonFormatting.js
+++ 22_jsonFormatting/solutions/jsonFormatting.js
@@ -1,12 +1,42 @@
 const booksAuthors = require('../books.json');
 
 const jsonFormatting = (obj) => {
-  const booksArr = obj.data;
-  let book = {};
-      author = {};
-      books = [];
+  const booksData = obj.data;
+  let books = [];
+  let authors = {};
 
-  return { books }
+  booksData.forEach(book => {
+
+    authors[book.book_id] = authors[book.book_id] || [];
+
+    books.push({
+      id: book.book_id,
+      title: book.title,
+      description: book.description,
+      genre: book.genre,
+      cover: book.cover,
+    });
+
+    authors[book.book_id].push({
+      first_name: book.first_name,
+      last_name: book.last_name,
+      biography: book.biography,
+      portrait: book.portrait
+    });
+
+  });
+
+  books = books.reduce((final, cVal, cIdx, arr) => {
+    if (arr[cIdx + 1] && cVal.id !== arr[cIdx + 1].id) {
+      cVal.authors = authors[cVal.id];
+      final.push(cVal);
+    } 
+
+    return final;
+
+  }, []);
+
+  return { books };
 
 };
 
