509ec50cbd8f03950b7e611e621dfcae82153790
Brandon Blair <brandon.d.blair@me.com>
2017-05-08 21:24:51 -0600

    building bones of array function

---
+++ 34_arrays/solutions/arrays.js
@@ -0,0 +1,11 @@
+function array() {
+  var newArray = {}
+  newArray.length = 0
+  newArray.push = function (value) {
+    //TODO
+  }
+  newArray.pop = function () {
+    //TODO
+  }
+  return newArray
+}
