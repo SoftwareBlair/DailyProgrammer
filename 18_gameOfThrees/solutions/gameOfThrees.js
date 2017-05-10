2b2ed32b3a79225e3432069ebdf7c2dc05db1790
Brandon Blair <brandon.d.blair@me.com>
2017-05-10 16:44:26 -0600

    Adding comments to explain some solutions

--- 18_gameOfThrees/solutions/gameOfThrees.js
+++ 18_gameOfThrees/solutions/gameOfThrees.js
@@ -1,25 +1,25 @@
 const gameOfThrees = (int) => {
-
+  // While input is greater than one
   while (int > 1) {
-
+    // If input divided by three has no remainder
     if (int % 3 === 0) {
-      console.log(`${int}: ${int} / 3 = ${int / 3}`);
-      int = int / 3;
+      console.log(`${int}: ${int} / 3 = ${int / 3}`); // Logs current number and the math being performed
+      int = int / 3; // Set input to input divided by three
     }
-    else
+    else // If input plus one and then divided by three has no remainder
     if ((int + 1) % 3 === 0) {
-      console.log(`${int}: (${int} + 1) / 3 = ${(int + 1) / 3}`);
-      int = (int + 1) / 3;
+      console.log(`${int}: (${int} + 1) / 3 = ${(int + 1) / 3}`); // Logs current number and the math being performed
+      int = (int + 1) / 3; // Set input to input plus one then divide by three
     }
-    else
+    else // If input minus one and then divided by three has no remainder
     if ((int - 1) % 3 === 0) {
-      console.log(`${int}: (${int} - 1) / 3 = ${(int - 1) / 3}`);
-      int = (int - 1) / 3;
+      console.log(`${int}: (${int} - 1) / 3 = ${(int - 1) / 3}`); // Logs current number and the math being performed
+      int = (int - 1) / 3; // Set input to input minus one then divide by three
     };
 
   };
 
-  console.log(`Result: ${int} \n`);
+  console.log(`Result: ${int} \n`); // Log input when it gets to one
   return int;
 
 };
