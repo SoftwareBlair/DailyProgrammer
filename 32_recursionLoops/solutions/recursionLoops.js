4f662c63854f718ddd6961e1f03ea9d7d16d3420
Brandon Blair <brandon.d.blair@me.com>
2017-05-10 17:00:36 -0600

    Adding comments to explain some solutions

--- 32_recursionLoops/solutions/recursionLoops.js
+++ 32_recursionLoops/solutions/recursionLoops.js
@@ -8,17 +8,18 @@
 
 // Non recursive solution
 const fizzBuzz = (int) => {
-  let numArray = Array.from(new Array(int), (v, i) => i + 1);
+  let numArray = Array.from(new Array(int), (v, i) => i + 1); // Create new array from inputed number
   let result = [];
 
+  // Loop through each element of array
   numArray.forEach(num => {
 
-    if (num % 15 === 0) result.push(`FizzBuzz`);
+    if (num % 15 === 0) result.push(`FizzBuzz`); // If number is divisible by 3 & 5
     else
-    if (num % 3 === 0) result.push(`Fizz`);
+    if (num % 3 === 0) result.push(`Fizz`); // If number is divisible by only 3
     else
-    if (num % 5 === 0) result.push(`Buzz`);
-    else result.push(num);
+    if (num % 5 === 0) result.push(`Buzz`); // // If number is divisible by only 5
+    else result.push(num); // Push number if doesn't meet conditionals
 
   });
 
@@ -30,13 +31,14 @@ const fizzBuzz = (int) => {
 const fizzBuzzRec = (int) => {
   let result = [];
 
-  if (int % 15 === 0) result.push('FizzBuzz');
+  if (int % 15 === 0) result.push('FizzBuzz'); // // If number is divisible by 3 & 5
   else
-  if (int % 3 === 0) result.push('Fizz');
+  if (int % 3 === 0) result.push('Fizz'); // If number is divisible by only 3
   else
-  if (int % 5 === 0) result.push('Buzz');
-  else result.push(int);
+  if (int % 5 === 0) result.push('Buzz'); // If number is divisible by only 5
+  else result.push(int); // Push number if it doesn't meet conditional
 
+  // Call function again decreasing input by one and join with result
   return fizzBuzz(int - 1).concat(result);
 
 };
